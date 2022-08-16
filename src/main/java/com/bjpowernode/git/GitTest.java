e26ad44f3b2540ef633d341ef4ef5eb57cfceeb0
seaSunshine-king <2670291804@qq.com>
2022-08-16 10:23:28 +0800

    push commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("-----------git-------------");
         System.out.println("----------master----------");
         System.out.println("------hot-fix------");
+        System.out.println("-------push test-------");
     }
 }
